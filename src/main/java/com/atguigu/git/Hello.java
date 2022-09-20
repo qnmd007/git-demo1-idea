c87ee75ba23d227d2e2c93cf9a18f41f65aa3c13
hhl <1827852024@qq.com>
2022-09-20 22:50:36 +0800

    合并后的代码

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
         System.out.println("第三次");
         System.out.println("77777777");
         System.out.println("master代码:88888");
+        System.out.println("master2代码:999");
         System.out.println("dev-A分支提交的代码");
     }
 }
